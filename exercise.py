b186d72464d85a65c97d5df6fa9c0e6504c6388a
Dimon-lucha <luchnikov3d@gmail.com>
2024-11-03 12:41:18 +0300

    "Пространство имен."

--- exercise.py
+++ exercise.py
@@ -1,10 +1,6 @@
-from fake_mach import divide as f
-from true_mach import divide as t
-res1 = f(69, 3)
-res2 = f(3, 0)
-res3 = t(49, 7)
-res4 = t(15, 0)
-print(res1)
-print(res2)
-print(res3)
-print(res4)
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+
+
+print(inner_function)# Ошибка
